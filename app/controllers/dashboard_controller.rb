a40a0ef28973a94f451e0435331241f7392e6379
Katrina Sanford <blueyed_qt@hotmail.com>
2015-02-27 10:29:57 -0800

    Fixed issue with permissions

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,7 +1,7 @@
 class DashboardController < ApplicationController
   def index
   	session[:oauth] = Koala::Facebook::OAuth.new(APP_ID, APP_SECRET, SITE_URL + '/dashboard/show')
-  	@auth_url = session[:oauth].url_for_oauth_code(:permissions=>"read_stream")
+  	@auth_url = session[:oauth].url_for_oauth_code(permissions: [:read_stream, :user_friends, :user_birthday, :email])
   	puts session.to_s + "<<< session"
   	respond_to do |format|
   		format.html {  }
@@ -14,13 +14,20 @@ class DashboardController < ApplicationController
   		session[:access_token] = session[:oauth].get_access_token(params[:code])
   	end
   	@api = Koala::Facebook::API.new(session[:access_token])
-  	# @graph_data = @api.get_object("/me/statuses", "fields"=>"message")
+  	
   	begin
-  		@profile = @api.get_object("/me")
-  		@friends = @api.get_object("/me/friends")
+  		@profile = @api.get_object("me")
+  		@friends = @api.get_object("me/friends")
+      # @email = @api.get_object("me/email")
   	end
   	respond_to do |format|
   		format.html {  }
   	end
+    puts 'USER ----------------------'
+    puts @profile
+    puts 'FRIENDS ----------------------'
+    puts @friends
+    # puts 'EMAIL ----------------------'
+    # puts @email
   end
 end
